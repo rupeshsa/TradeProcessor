c1e2ba8b068a2724c3c2fd9713cbaa9c94031b04
rupeshsa <rupesh.ms@gmail.com>
2022-06-26 11:28:16 +0530

    Updated files

--- src/com/rs/trades/handler/test/TradeHandlerTest.java
+++ src/com/rs/trades/handler/test/TradeHandlerTest.java
@@ -18,7 +18,7 @@ public class TradeHandlerTest {
 	
 	@Test
 	public void validateHappyPathTest() throws Exception {
-		Trade trade= new Trade("T1", 1, "CP-1", "B1", sdf.parse("20/05/2020"), new Date(), "N");
+		Trade trade= new Trade("T1", 1, "CP-1", "B1", sdf.parse("20/05/2023"), new Date(), "N");
 		ConcurrentHashMap<String, Trade> tradeMap = handler.validateVersionSequence(trade);
 		assertEquals(1,tradeMap.size());
 	}
@@ -27,7 +27,7 @@ public class TradeHandlerTest {
 	@SuppressWarnings("unused")
 	@Test(expected = Exception.class) 
 	public void validateLowerVersionTest() throws Exception {
-		Trade trade= new Trade("T2", 2, "CP-2", "B1", sdf.parse("20/05/2021"), new Date(), "N");
+		Trade trade= new Trade("T2", 2, "CP-2", "B1", sdf.parse("20/05/2023"), new Date(), "N");
 		ConcurrentHashMap<String, Trade> tradeMap = handler.validateVersionSequence(trade);
 		
 		trade= new Trade("T2", 1, "CP-1", "B1", sdf.parse("20/05/2021"), sdf.parse("14/03/2015"), "N");
